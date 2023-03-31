528a80ed95d5fbcea7838edd5ccdba276c763f3d
Baltruck <optimix@live.ca>
2023-03-31 12:22:46 -0400

    Update record.js

--- mern/server/routes/record.js
+++ mern/server/routes/record.js
@@ -1,36 +1,31 @@
 const express = require("express");
-const {UserModel, PostModel, CommentModel} = require("../db/schemas") //import schemas
-const validator = require('validator');
-const { v4: uuidv4 } = require('uuid');
+const { UserModel, PostModel, CommentModel } = require("../db/schemas");
+const validator = require("validator");
+const { v4: uuidv4 } = require("uuid");
 
 const recordRoutes = express.Router();
 
-// This will help us connect to the database
 const dbo = require("../db/conn");
 const { response } = require("express");
 const e = require("express");
 
-// This help convert the id from string to ObjectId for the _id.
 const ObjectId = require("mongodb").ObjectId;
 
 // Middlewares Definitions
 const emailValidator = (req, res, next) => {
   const email = req.body.email;
   if (validator.isEmail(email)) {
-    // console.log('Email is valid!');
     next();
   } else {
-    // console.log('Email is not valid.');
-    return res.send({message: "Invalid email"})
+    return res.send({ message: "Invalid email" });
   }
-  
 };
 
 // Middleware CAll
 recordRoutes.use("/signup", emailValidator);
 
 // User creation and login /********************** *//********************** *//********************** */
-recordRoutes.route("/signup").post( async (req, response) => {
+recordRoutes.route("/signup").post(async (req, response) => {
   let db_connect = dbo.getDb();
   const user = new UserModel({
     first_name: req.body.first_name,
@@ -42,7 +37,9 @@ recordRoutes.route("/signup").post( async (req, response) => {
     occupation: req.body.occupation,
   });
 
-  const notANewUser = await db_connect.collection("User").findOne( {"email": user.email});
+  const notANewUser = await db_connect
+    .collection("User")
+    .findOne({ email: user.email });
 
   if (!notANewUser) {
     await db_connect.collection("User").insertOne(user, function (err, res) {
@@ -54,44 +51,38 @@ recordRoutes.route("/signup").post( async (req, response) => {
   }
 });
 
-recordRoutes.route("/login").post( async (req, response) => {
+recordRoutes.route("/login").post(async (req, response) => {
   let db_connect = dbo.getDb();
-  const {email, password} = req.body;
+  const { email, password } = req.body;
   const user = await db_connect.collection("User").findOne({ email });
   if (!user) {
     return response.status(400).json({ message: "No user found" });
-  } else if ( user && password !== user.password) {
+  } else if (user && password !== user.password) {
     return response.status(400).json({ message: "Wrong password" });
   }
-  await db_connect.collection("Session").deleteMany({ ["user.email"]: user.email });
+  await db_connect
+    .collection("Session")
+    .deleteMany({ ["user.email"]: user.email });
   const session_token = uuidv4();
-  const session = { session_id: session_token,
-     user: {_id: user._id,first_name: user.first_name, last_name: user.last_name, birthday: user.birthday, email: user.email, password: user.password, status: user.status, location: user.location, occupation: user.occupation, auth_level: user.auth_level},
-      session_date: new Date() };
+  const session = {
+    session_id: session_token,
+    user: {
+      _id: user._id,
+      first_name: user.first_name,
+      last_name: user.last_name,
+      birthday: user.birthday,
+      email: user.email,
+      password: user.password,
+      status: user.status,
+      location: user.location,
+      occupation: user.occupation,
+      auth_level: user.auth_level,
+    },
+    session_date: new Date(),
+  };
   await db_connect.collection("Session").insertOne(session);
-  return response.send({message: "Login Successful", User: session});
+  return response.send({ message: "Login Successful", User: session });
 });
-
-// recordRoutes.route("/logout").get( async (req, res) => {
-
-//   let db_connect = dbo.getDb();
-//   let cacheToken = req.query.token; //grabs the token from the browser
-//   console.log("TOKEN")
-//   console.log(cacheToken);
-//   db_connect
-//   .collection("Session")
-//   .deleteOne({session_id: cacheToken}, function (err, result) {
-//     if (err) {
-//       res.status(500).json({ status: "error", message: "Failed to delete token" });
-//     } else if (!result) {
-//       res.json({status: "ok", data: {valid: false, user: null, message: "Invalid token"}});
-//     } else {
-//       res.json({status: "ok", data: { message: "Session deleted"}});
-//     }
-// });
-
-
-//   await db_connect.collection("Session").deleteMany({ ["user.email"]: user.email });
 
 // User creation and login /********************** *//********************** *//********************** */
 
@@ -99,30 +90,32 @@ recordRoutes.route("/status").patch(async (req, response) => {
   let db_connect = dbo.getDb();
 
   if (!ObjectId.isValid(req.body.userId)) {
-    console.log('Invalid userId'); 
+    console.log("Invalid userId");
     return response.status(400).json({ message: "Invalid userId" });
   }
+
   const userId = ObjectId(req.body.userId);
   const statusUpdate = req.body.status;
 
-  const user = await db_connect.collection("User").findOne({ _id: userId});
+  const user = await db_connect.collection("User").findOne({ _id: userId });
 
   if (!user) {
     return response.status(400).json({ message: "No user found" });
   } else {
-    await db_connect.collection("User").updateOne(
-      { _id: userId },
-      { $set: { "status": statusUpdate } }
-    );
+    await db_connect
+      .collection("User")
+      .updateOne({ _id: userId }, { $set: { status: statusUpdate } });
     response.send({ message: "Status Updated", newStatus: statusUpdate });
   }
 });
 // Article (Blog Post) Section /********************** *//********************** *//********************** */
-recordRoutes.route("/new-article").post( async (req, response ) => {
+recordRoutes.route("/new-article").post(async (req, response) => {
   let db_connect = dbo.getDb();
   const userEmail = req.body.email;
   const content = req.body.content;
-  const user = await db_connect.collection("User").findOne({ email: userEmail });
+  const user = await db_connect
+    .collection("User")
+    .findOne({ email: userEmail });
   const currentDate = new Date();
 
   const article = new PostModel({
@@ -130,7 +123,7 @@ recordRoutes.route("/new-article").post( async (req, response ) => {
     user_id: user._id,
     time_stamp: currentDate,
   });
-  console.log(article)
+  console.log(article);
   db_connect.collection("Post").insertOne(article, function (err, res) {
     if (err) throw err;
     response.send(article);
@@ -140,11 +133,10 @@ recordRoutes.route("/new-article").post( async (req, response ) => {
 recordRoutes.route("/get-articles").post(async (req, response) => {
   let db_connect = dbo.getDb("CodeBlogg");
   if (!ObjectId.isValid(req.body.userId)) {
-    console.log('Invalid userId'); 
+    console.log("Invalid userId");
     return response.status(400).json({ message: "Invalid userId" });
   }
   const userId = ObjectId(req.body.userId);
-  // console.log("reqbody", req.body.userId);
 
   const user = await db_connect.collection("User").findOne({ _id: userId });
 
@@ -164,32 +156,30 @@ recordRoutes.route("/get-articles").post(async (req, response) => {
   }
 });
 
-recordRoutes.route("/get-posts").get( (req, response) => {//returns all posts
+recordRoutes.route("/get-posts").get((req, response) => {
   let db_connect = dbo.getDb("CodeBlogg");
   db_connect
-  .collection("Post")
-  .find({})
-  .toArray(function (err, result) {
-    if (err) throw err;
-    response.json(result);
-  });
-
-})
+    .collection("Post")
+    .find({})
+    .toArray(function (err, result) {
+      if (err) throw err;
+      response.json(result);
+    });
+});
 // Comment Section /********************** *//********************** *//********************** */
-recordRoutes.route("/new-comment").post( async (req, response ) => {
-
+recordRoutes.route("/new-comment").post(async (req, response) => {
   let db_connect = dbo.getDb();
 
-  //Post commented on
   const post_id = ObjectId(req.body.post_id);
-  //User making the comment
   const userEmail = req.body.email;
 
-  const user = await db_connect.collection("User").findOne( {email: userEmail});
+  const user = await db_connect
+    .collection("User")
+    .findOne({ email: userEmail });
   console.log("USER");
   console.log(user);
 
-  const post = await db_connect.collection("Post").findOne( {_id: post_id});
+  const post = await db_connect.collection("Post").findOne({ _id: post_id });
   console.log("POST content");
   console.log(post);
 
@@ -210,30 +200,28 @@ recordRoutes.route("/new-comment").post( async (req, response ) => {
   });
 
   console.log(comment);
-  //append the comment to the post comment list
-  await db_connect.collection("Post").updateOne(
-    { _id: post_id}, //post._id not userid
-    { $push: { comments: comment } }
-    );
-  //add the comment to the Comment conllection
-  await db_connect.collection("Comment").insertOne(comment, function (err, res) {
+  await db_connect
+    .collection("Post")
+    .updateOne({ _id: post_id }, { $push: { comments: comment } });
+  await db_connect
+    .collection("Comment")
+    .insertOne(comment, function (err, res) {
       if (err) throw err;
-      response.send( {comment});
+      response.send({ comment });
     });
 });
-//Get user informations
 recordRoutes.route("/userInfo").post(async (req, response) => {
   let db_connect = dbo.getDb();
 
   if (!ObjectId.isValid(req.body.userId)) {
-    console.log('Invalid userId'); 
+    console.log("Invalid userId");
     return response.status(400).json({ message: "Invalid userId" });
   }
-  console.log("You PASSED THE TEST")
+  console.log("You PASSED THE TEST");
   const userId = ObjectId(req.body.userId);
   const user = await db_connect.collection("User").findOne({ _id: userId });
   if (!user) {
-    console.log('User not found'); 
+    console.log("User not found");
     return response.status(404).json({ message: "User not found" });
   }
   response.send({ UserInfo: user });
@@ -242,10 +230,9 @@ recordRoutes.route("/userInfo").post(async (req, response) => {
 recordRoutes.route("/userOfPost/:userId").get(async (req, response) => {
   let db_connect = dbo.getDb();
   const userId = ObjectId(req.params.userId);
-  // console.log(req.params.userId);
   const user = await db_connect.collection("User").findOne({ _id: userId });
   if (!user) {
-    console.log('User not found'); // Log user not found
+    console.log("User not found");
     return response.status(404).json({ message: "User not found" });
   }
   response.send({ UserInfo: user });
@@ -254,84 +241,70 @@ recordRoutes.route("/userOfPost/:userId").get(async (req, response) => {
 recordRoutes.route("/findUserId").post(async (req, response) => {
   let db_connect = dbo.getDb();
   if (!ObjectId.isValid(req.body.userId)) {
-    console.log('Invalid userId'); 
+    console.log("Invalid userId");
     return response.status(400).json({ message: "Invalid userId" });
   }
   const userId = ObjectId(req.body.userId);
   const user = await db_connect.collection("User").findOne({ _id: userId });
   if (!user) {
-    console.log('User not found'); 
+    console.log("User not found");
     return response.status(404).json({ message: "User not found" });
   }
   response.send({ UserInfo: user });
 });
 
-
 recordRoutes.route("/get-all-users").get(async (req, response) => {
   let db_connect = dbo.getDb("CodeBlogg");
   db_connect
-  .collection("User")
-  .find({})
-  .toArray(function (err, result) {
-    if (err) throw err;
-    response.json(result);
-  });
+    .collection("User")
+    .find({})
+    .toArray(function (err, result) {
+      if (err) throw err;
+      response.json(result);
+    });
 });
-
-// Likes Section /********************** *//********************** *//********************** */
-//     //update the comment collection with the new likes
-//     await db_connect.collection("Comment").updateOne(
-//       {_id: comment_id, "post_id._id": post_id._id},
-//       {$set: {"post_id.likes": updatedPost.likes}}
-//     );
-    
-//     response.send( {message: "Post  Liked"});
-      
-// });
-
 
 recordRoutes.route("/like").patch(async (req, response) => {
   let db_connect = dbo.getDb();
   const post_id = ObjectId(req.body.post_id);
 
-  // Rechercher le post correspondant à post_id dans la collection "Post" de la base de données
   const post = await db_connect.collection("Post").find({ _id: post_id });
   if (!post) {
     return response.status(400).json({ message: "Invalid Post" });
   }
 
-  // Mettre à jour le post en augmentant le nombre de likes (`likes`) de 1
-  await db_connect.collection("Post").updateOne(
-    { _id: post_id },
-    { $inc: { likes: 1 } }
-  );
+  await db_connect
+    .collection("Post")
+    .updateOne({ _id: post_id }, { $inc: { likes: 1 } });
 
   response.send({ message: "Post Liked" });
 });
 
-recordRoutes.route("/like-comment").patch( async (req, response) => {
-
+recordRoutes.route("/like-comment").patch(async (req, response) => {
   let db_connect = dbo.getDb();
   const post_id = ObjectId(req.body.post_id);
   const comment_id = ObjectId(req.body.comment_id);
-  const post = await db_connect.collection("Post").findOne( {_id: post_id}); 
+  const post = await db_connect.collection("Post").findOne({ _id: post_id });
   if (!post) {
     return response.status(400).json({ message: "Invalid Post" });
   }
 
-  await db_connect.collection("Comment").updateOne(
-    { _id: comment_id},
-    { $inc: { likes: 1 } }
+  await db_connect
+    .collection("Comment")
+    .updateOne({ _id: comment_id }, { $inc: { likes: 1 } });
+
+  const updatedComment = await db_connect
+    .collection("Comment")
+    .findOne({ _id: comment_id });
+
+  await db_connect
+    .collection("Post")
+    .updateOne(
+      { _id: post_id, "comments._id": comment_id },
+      { $set: { "comments.$.likes": updatedComment.likes } }
     );
 
-  const updatedComment = await db_connect.collection("Comment").findOne({_id: comment_id});
-
-  await db_connect.collection("Post").updateOne(
-    {_id: post_id, "comments._id": comment_id},
-    {$set: {"comments.$.likes": updatedComment.likes}}
-  );
-
-  return response.send( {message: "Comment Liked"}); //updater la collection post avec le nouveau like
+  return response.send({ message: "Comment Liked" });
 });
 
 recordRoutes.route("/update-user").patch(function (req, response) {
@@ -349,39 +322,37 @@ recordRoutes.route("/update-user").patch(function (req, response) {
       occupation: req.body.occupation,
       status: req.body.status,
       auth_level: req.body.auth_level,
-      },
+    },
   };
   db_connect
     .collection("User")
-    .updateOne({_id: userID}, newvalues, function (err, res) {
+    .updateOne({ _id: userID }, newvalues, function (err, res) {
       if (err) throw err;
       console.log("1 document updated");
       response.json(res);
     });
- });
+});
 
 recordRoutes.route("/delete-user").delete((req, response) => {
   let db_connect = dbo.getDb();
-  // let myquery = { _id: ObjectId(req.params.id) };
   const userID = ObjectId(req.body.user_id);
   console.log(userID);
-  db_connect.collection("User").deleteOne( {_id: userID}, function (err, obj) {
+  db_connect.collection("User").deleteOne({ _id: userID }, function (err, obj) {
     if (err) throw err;
     console.log("1 document deleted");
     response.json(obj);
   });
- });
+});
 
- recordRoutes.route("/delete-post").delete((req, response) => {
+recordRoutes.route("/delete-post").delete((req, response) => {
   let db_connect = dbo.getDb();
-  // let myquery = { _id: ObjectId(req.params.id) };
   const postID = ObjectId(req.body.post_id);
   console.log(postID);
-  db_connect.collection("Post").deleteOne( {_id: postID}, function (err, obj) {
+  db_connect.collection("Post").deleteOne({ _id: postID }, function (err, obj) {
     if (err) throw err;
     console.log("1 document deleted");
     response.json(obj);
   });
- });
+});
 
 module.exports = recordRoutes;
